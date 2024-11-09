a0aecce42135964ff4b65fb88b86b5bd5a0d28eb
Adeesha Gunawardana <arunodagunawardana@gmail.com>
2024-11-09 23:28:27 +0530

    Refactored ResponseDto with @ALlArgsConstructor Annotation.

--- src/main/java/com/adeesha/loans_microservice/dto/ResponseDto.java
+++ src/main/java/com/adeesha/loans_microservice/dto/ResponseDto.java
@@ -1,8 +1,10 @@
 package com.adeesha.loans_microservice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ResponseDto {
     private String statusCode;
 
