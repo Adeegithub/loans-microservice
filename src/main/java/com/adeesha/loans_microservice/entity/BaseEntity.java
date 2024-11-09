64f02c9618b87c42f083c801e7f87aeb3e4d5f4a
Adeesha Gunawardana <arunodagunawardana@gmail.com>
2024-11-09 22:13:04 +0530

    Implemented the BaseEntity

---
+++ src/main/java/com/adeesha/loans_microservice/entity/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.adeesha.loans_microservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@MappedSuperclass
+//Indicated the Spring Data JPA framework that this class is going to act as a
+//superclass for all the entities.
+public class BaseEntity {
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(updatable = false)
+    private String createdBy;
+
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(insertable = false)
+    private String updatedBy;
+}
