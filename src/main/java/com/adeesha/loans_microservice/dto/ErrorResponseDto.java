da8955c79ac7a86d2215c71d7de1e31a613fe8bc
Adeesha Gunawardana <arunodagunawardana@gmail.com>
2024-11-10 13:28:37 +0530

    Refactored ErrorResponseDto with @AllArgsConstructor

--- src/main/java/com/adeesha/loans_microservice/dto/ErrorResponseDto.java
+++ src/main/java/com/adeesha/loans_microservice/dto/ErrorResponseDto.java
@@ -1,11 +1,13 @@
 package com.adeesha.loans_microservice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
 public class ErrorResponseDto {
     private String apiPath;
 
